5832e0844b34610dd55f1c5dbdc829100fe477c3
RedstoneParadox <spinjitzu00@gmail.com>
2019-07-27 17:23:01 -0700

    Added dispenser behaviors for planting crops.

--- src/main/java/net/redstoneparadox/nicetohave/misc/DispenserBehaviors.kt
+++ src/main/java/net/redstoneparadox/nicetohave/misc/DispenserBehaviors.kt
@@ -1,7 +1,11 @@
 package net.redstoneparadox.nicetohave.misc
 
+import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.block.Blocks
 import net.minecraft.block.DispenserBlock
 import net.minecraft.block.dispenser.DispenserBehavior
+import net.minecraft.block.dispenser.ItemDispenserBehavior
 import net.minecraft.block.dispenser.ProjectileDispenserBehavior
 import net.minecraft.client.network.packet.EntityPositionS2CPacket
 import net.minecraft.entity.projectile.Projectile
@@ -37,9 +41,48 @@ object DispenserBehaviors {
                 return stack
             }
         })
+
+        register(net.minecraft.item.Items.WHEAT_SEEDS, PlantingDispenserBehavior(Blocks.FARMLAND, Blocks.WHEAT))
+        register(net.minecraft.item.Items.BEETROOT_SEEDS, PlantingDispenserBehavior(Blocks.FARMLAND, Blocks.BEETROOTS))
+        register(net.minecraft.item.Items.MELON_SEEDS, PlantingDispenserBehavior(Blocks.FARMLAND, Blocks.MELON_STEM))
+        register(net.minecraft.item.Items.PUMPKIN_SEEDS, PlantingDispenserBehavior(Blocks.FARMLAND, Blocks.PUMPKIN_STEM))
+        register(net.minecraft.item.Items.CHORUS_FLOWER, PlantingDispenserBehavior(Blocks.END_STONE, Blocks.CHORUS_FLOWER))
+        register(net.minecraft.item.Items.SUGAR_CANE, PlantingDispenserBehavior(arrayOf(Blocks.SAND, Blocks.DIRT), Blocks.SUGAR_CANE))
+        val bambooFarmBlocks = arrayOf(Blocks.GRASS_BLOCK, Blocks.DIRT, Blocks.SAND, Blocks.GRAVEL, Blocks.MYCELIUM, Blocks.PODZOL, Blocks.COARSE_DIRT, Blocks.RED_SAND)
+        register(net.minecraft.item.Items.BAMBOO, PlantingDispenserBehavior(bambooFarmBlocks, Blocks.BAMBOO_SAPLING))
+        val saplingFarmBlocks = arrayOf(Blocks.DIRT, Blocks.PODZOL, Blocks.GRASS_BLOCK, Blocks.FARMLAND)
+        val saplingFarmBlocks2 = arrayOf(Blocks.DIRT, Blocks.PODZOL, Blocks.GRASS_BLOCK)
+        register(net.minecraft.item.Items.OAK_SAPLING, PlantingDispenserBehavior(saplingFarmBlocks, Blocks.OAK_SAPLING))
+        register(net.minecraft.item.Items.BIRCH_SAPLING, PlantingDispenserBehavior(saplingFarmBlocks, Blocks.BIRCH_SAPLING))
+        register(net.minecraft.item.Items.JUNGLE_SAPLING, PlantingDispenserBehavior(saplingFarmBlocks, Blocks.JUNGLE_SAPLING))
+        register(net.minecraft.item.Items.SPRUCE_SAPLING, PlantingDispenserBehavior(saplingFarmBlocks, Blocks.SPRUCE_SAPLING))
+        register(net.minecraft.item.Items.ACACIA_SAPLING, PlantingDispenserBehavior(saplingFarmBlocks2, Blocks.ACACIA_SAPLING)) 
+        register(net.minecraft.item.Items.DARK_OAK_SAPLING, PlantingDispenserBehavior(saplingFarmBlocks2, Blocks.DARK_OAK_SAPLING))
     }
 
     fun register(item : Item, behavior : DispenserBehavior) {
         DispenserBlock.registerBehavior(item, behavior)
+    }
+
+    class PlantingDispenserBehavior(val farmlandBlocks : Array<Block>, val plant : Block) : ItemDispenserBehavior() {
+
+        constructor(farmland: Block, plant: Block) : this(arrayOf(farmland), plant)
+
+        override fun dispenseSilently(pointer: BlockPointer?, itemStack: ItemStack?): ItemStack {
+            var stack = itemStack!!
+            var world = pointer!!.world
+            var direction = pointer.blockState.get(DispenserBlock.FACING)
+            var farmBlock = world.getBlockState(pointer.blockPos.offset(direction).down()).block
+
+            for (block in farmlandBlocks) {
+                if (farmBlock == block && world.getBlockState(pointer.blockPos.offset(direction)).block == Blocks.AIR) {
+                    world.setBlockState(pointer.blockPos.offset(direction), plant.defaultState)
+                    stack.decrement(1)
+                    return stack
+                }
+            }
+
+            return super.dispenseSilently(pointer, stack)
+        }
     }
 }
