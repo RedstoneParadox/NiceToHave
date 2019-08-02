9adfa37bce9e3f7c7d5b3ba1489948c42b5e7ce2
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-02 09:38:11 -0700

    Client side initializer now implements the correct interface

--- src/main/java/net/redstoneparadox/nicetohave/client/NiceToHaveClient.kt
+++ src/main/java/net/redstoneparadox/nicetohave/client/NiceToHaveClient.kt
@@ -1,14 +1,15 @@
 package net.redstoneparadox.nicetohave.client
 
+import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.ModInitializer
 import net.redstoneparadox.nicetohave.client.render.entity.EntityRenderers
 
 /**
  * Created by RedstoneParadox on 5/24/2019.
  */
-class NiceToHaveClient : ModInitializer {
+class NiceToHaveClient : ClientModInitializer {
 
-    override fun onInitialize() {
+    override fun onInitializeClient() {
         println("Greetings from the client!")
         EntityRenderers.registerRenderers()
     }
