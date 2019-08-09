522718b8493e51870f1b97875566adaf03e2b1af
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-08 18:58:23 -0700

    Misc changes

--- src/main/java/net/redstoneparadox/nicetohave/NiceToHave.kt
+++ src/main/java/net/redstoneparadox/nicetohave/NiceToHave.kt
@@ -1,20 +1,15 @@
 package net.redstoneparadox.nicetohave
 
 import net.fabricmc.api.ModInitializer
-import net.fabricmc.loader.FabricLoader
 import net.minecraft.util.registry.Registry
-import net.minecraft.world.World
 import net.redstoneparadox.nicetohave.block.Blocks
-import net.redstoneparadox.nicetohave.enchantment.Enchantments
 import net.redstoneparadox.nicetohave.entity.EntityTypes
 import net.redstoneparadox.nicetohave.entity.effect.StatusEffects
 import net.redstoneparadox.nicetohave.item.Items
 import net.redstoneparadox.nicetohave.item.wrench.WrenchItem
 import net.redstoneparadox.nicetohave.misc.DispenserBehaviors
 import net.redstoneparadox.nicetohave.misc.Listeners
-import net.redstoneparadox.nicetohave.networking.Packets
 import net.redstoneparadox.nicetohave.potion.Potions
-import net.redstoneparadox.nicetohave.util.Config
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import net.redstoneparadox.nicetohave.world.biome.Biomes
