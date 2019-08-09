1a915b62e14ed33a8cd917f3022eded8beb61b7c
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-09 16:58:39 -0700

    Replaced hacky reflection with potion registry accessor

--- src/main/java/net/redstoneparadox/nicetohave/potion/PotionRecipes.java
+++ src/main/java/net/redstoneparadox/nicetohave/potion/PotionRecipes.java
@@ -5,6 +5,7 @@ import net.minecraft.item.Items;
 import net.minecraft.potion.Potion;
 import net.minecraft.recipe.BrewingRecipeRegistry;
 import net.minecraft.recipe.Ingredient;
+import net.redstoneparadox.nicetohave.mixin.recipe.BrewingRecipeRegistryAccessor;
 import net.redstoneparadox.nicetohave.util.Config;
 
 import java.lang.reflect.Constructor;
@@ -14,28 +15,6 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 public class PotionRecipes {
-
-    private static Method addMethod;
-    private static Object listInstance;
-    private static Constructor<?> recipeConstructor;
-
-    static {
-        try {
-            Field potion_recipes_field = BrewingRecipeRegistry.class.getDeclaredField("POTION_RECIPES");
-            potion_recipes_field.setAccessible(true);
-            listInstance = potion_recipes_field.get(null);
-            Class<List<?>> listClass = (Class<List<?>>) listInstance.getClass();
-            addMethod = listClass.getDeclaredMethod("add", Object.class);
-            addMethod.setAccessible(true);
-            Class<?> recipeClass = Class.forName("net.minecraft.recipe.BrewingRecipeRegistry$Recipe");
-            recipeConstructor = recipeClass.getDeclaredConstructor(Object.class, Ingredient.class, Object.class);
-            recipeConstructor.setAccessible(true);
-        }
-        catch (ClassNotFoundException | NoSuchFieldException | NoSuchMethodException | IllegalAccessException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-    }
 
     public static void registerRecipes() {
         register("insight", net.minecraft.potion.Potions.THICK, Items.EXPERIENCE_BOTTLE, Potions.INSTANCE.getINSIGHT());
@@ -49,11 +28,7 @@ public class PotionRecipes {
 
     private static void register(String configKey, Potion base, Item ingredient, Potion result) {
         if (Config.INSTANCE.getPotionOption(configKey, Boolean.class, true)) {
-            try {
-                addMethod.invoke(listInstance, recipeConstructor.newInstance(base, Ingredient.ofItems(ingredient), result));
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                e.printStackTrace();
-            }
+            BrewingRecipeRegistryAccessor.invokeRegisterPotionRecipe(base, ingredient, result);
         }
     }
 }
