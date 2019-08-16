94359b8c32568fe634912033f875bddbcc04d03c
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-16 14:49:18 -0700

    Drinks now give you back the bottle when drunk

--- src/main/java/redstoneparadox/nicetohave/item/DrinkItem.kt
+++ src/main/java/redstoneparadox/nicetohave/item/DrinkItem.kt
@@ -1,5 +1,25 @@
 package redstoneparadox.nicetohave.item
 
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
+import net.minecraft.world.World
 
-class DrinkItem(settings: Settings) : Item(settings)
+class DrinkItem(settings: Settings) : Item(settings) {
+
+    override fun finishUsing(itemStack: ItemStack, world: World, livingEntity: LivingEntity): ItemStack {
+        val eatenStack = super.finishUsing(itemStack, world, livingEntity);
+        if (livingEntity is PlayerEntity && !livingEntity.isCreative) {
+            if (eatenStack.isEmpty) {
+                return ItemStack(Items.GLASS_BOTTLE)
+            }
+            else {
+                livingEntity.inventory.insertStack(ItemStack(Items.GLASS_BOTTLE))
+            }
+        }
+
+        return eatenStack
+    }
+}
