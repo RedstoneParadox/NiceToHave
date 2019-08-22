e1098a9c21764d4e6ca5581b1ed0b4c40f9b7a7a
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-21 17:36:21 -0700

    Created helper classes for generating blockstate files

---
+++ src/main/java/redstoneparadox/nicetohave/util/datagen/VariantBlockStateBuilder.kt
@@ -0,0 +1,78 @@
+package redstoneparadox.nicetohave.util.datagen
+
+import blue.endless.jankson.JsonObject
+import blue.endless.jankson.JsonPrimitive
+import net.fabricmc.loader.api.FabricLoader
+import redstoneparadox.nicetohave.NiceToHave
+import java.io.File
+
+class VariantBlockStateBuilder {
+
+    private var id: String = ""
+    private var namespace: String = "nicetohave"
+    private val variants: HashMap<String, VariantModelBuilder> = HashMap()
+
+    // Directory
+    private val currentDirectory: File?
+        get() = if (FabricLoader.getInstance().isDevelopmentEnvironment) {
+            File(FabricLoader.getInstance().gameDirectory.parentFile, "..\\src\\main\\resources\\assets\\$namespace\\blockstates")
+        } else {
+            null
+        }
+
+    fun setID(id: String): VariantBlockStateBuilder {
+        this.id = id
+        return this
+    }
+
+    fun setNamespace(namespace: String): VariantBlockStateBuilder {
+        this.namespace = namespace
+        return this
+    }
+
+    fun addVariant(variantString: String, model: VariantModelBuilder): VariantBlockStateBuilder {
+        variants[variantString] = model
+        return this
+    }
+
+    fun save() {
+        if (currentDirectory == null) {
+            NiceToHave.warn("Attempted to generate data outside of dev environment.")
+            return
+        }
+
+        val blockStateJson = JsonObject()
+        val variantsJson = JsonObject()
+        for (variantsEntry in variants) {
+            variantsJson[variantsEntry.key] = variantsEntry.value.toJson()
+        }
+        blockStateJson["variants"] = variantsJson
+
+        val blockStateString = blockStateJson.toJson(false, true)
+        File(currentDirectory, "$id.json").bufferedWriter().use { it.write(blockStateString) }
+    }
+
+    class VariantModelBuilder(val model: String, val x: Int = 0, val y: Int = 0, val z : Int = 0) {
+
+        fun toJson(): JsonObject {
+            val variantJson = JsonObject()
+            variantJson["model"] = JsonPrimitive(model)
+
+            if (x != 0) {
+                variantJson["x"] = JsonPrimitive(x)
+            }
+
+            if (y != 0) {
+                variantJson["y"] = JsonPrimitive(y)
+            }
+
+            if (z != 0) {
+                variantJson["z"] = JsonPrimitive(0)
+            }
+
+            return variantJson
+        }
+    }
+
+
+}
