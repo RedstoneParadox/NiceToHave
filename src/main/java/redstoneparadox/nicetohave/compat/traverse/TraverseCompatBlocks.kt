39245f2f555714e3c87f0511d21f7ad6df1b94d4
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-22 15:24:20 -0700

    Added stripped fir pole

--- src/main/java/redstoneparadox/nicetohave/compat/traverse/TraverseCompatBlocks.kt
+++ src/main/java/redstoneparadox/nicetohave/compat/traverse/TraverseCompatBlocks.kt
@@ -9,10 +9,11 @@ import redstoneparadox.nicetohave.util.initializers.BlocksInitializer
 object TraverseCompatBlocks : BlocksInitializer() {
 
     val FIR_POLE : PoleBlock? = registerPole(TraverseBlocks.FIR_WOOD, "fir", "traverse")
+    val STRIPPED_FIR_POLE : PoleBlock? = registerPole(TraverseBlocks.FIR_WOOD, "stripped_fir", "traverse")
 
     fun initBlocks() {
         if (Config.getBool("blocks.poles")) {
-            registerFlammableBlock(FIR_POLE, FlammableBlockRegistry.Entry(5, 20))
+            registerFlammableBlocks(arrayOf(FIR_POLE, STRIPPED_FIR_POLE), FlammableBlockRegistry.Entry(5, 20))
         }
     }
 }
--- src/main/java/redstoneparadox/nicetohave/compat/traverse/TraverseCompatItems.kt
+++ src/main/java/redstoneparadox/nicetohave/compat/traverse/TraverseCompatItems.kt
@@ -7,10 +7,11 @@ import redstoneparadox.nicetohave.util.initializers.ItemsInitializer
 object TraverseCompatItems : ItemsInitializer() {
 
     val FIR_POLE : BlockItem? = registerPoleItem(TraverseCompatBlocks.FIR_POLE, "fir")
+    val STRIPPED_FIR_POLE : BlockItem? = registerPoleItem(TraverseCompatBlocks.STRIPPED_FIR_POLE, "stripped_fir")
 
     fun initItems() {
         if (Config.getBool("blocks.poles")) {
-            registerFuel(FIR_POLE, 300)
+            registerFuelForEach(arrayOf(STRIPPED_FIR_POLE, FIR_POLE), 300)
         }
     }
 }
