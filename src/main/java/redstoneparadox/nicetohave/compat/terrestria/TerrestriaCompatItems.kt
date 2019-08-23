6321c8d04af011fddec848d30d30d4bcf4a7b217
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-23 16:14:33 -0700

    Terrestria's stripped poles are now registered with the right block

--- src/main/java/redstoneparadox/nicetohave/compat/terrestria/TerrestriaCompatItems.kt
+++ src/main/java/redstoneparadox/nicetohave/compat/terrestria/TerrestriaCompatItems.kt
@@ -16,14 +16,14 @@ object TerrestriaCompatItems : ItemsInitializer() {
     val RAINBOW_EUCALYPTUS_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.RAINBOW_EUCALYPTUS_POLE, "rainbow_eucalyptus")
     val SAKURA_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.SAKURA_POLE, "sakura")
 
-    val STRIPPED_REDWOOD_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.REDWOOD_POLE, "stripped_redwood")
-    val STRIPPED_HEMLOCK_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.HEMLOCK_POLE, "stripped_hemlock")
-    val STRIPPED_RUBBER_WOOD_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.RUBBER_WOOD_POLE, "stripped_rubber")
-    val STRIPPED_CYPRESS_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.CYPRESS_POLE, "stripped_cypress")
-    val STRIPPED_WILLOW_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.WILLOW_POLE, "stripped_willow")
-    val STRIPPED_JAPANESE_MAPLE_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.JAPANESE_MAPLE_POLE, "stripped_japanese_maple")
-    val STRIPPED_RAINBOW_EUCALYPTUS_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.RAINBOW_EUCALYPTUS_POLE, "stripped_rainbow_eucalyptus")
-    val STRIPPED_SAKURA_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.SAKURA_POLE, "stripped_sakura")
+    val STRIPPED_REDWOOD_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.STRIPPED_REDWOOD_POLE, "stripped_redwood")
+    val STRIPPED_HEMLOCK_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.STRIPPED_HEMLOCK_POLE, "stripped_hemlock")
+    val STRIPPED_RUBBER_WOOD_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.STRIPPED_RUBBER_WOOD_POLE, "stripped_rubber")
+    val STRIPPED_CYPRESS_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.STRIPPED_CYPRESS_POLE, "stripped_cypress")
+    val STRIPPED_WILLOW_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.STRIPPED_WILLOW_POLE, "stripped_willow")
+    val STRIPPED_JAPANESE_MAPLE_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.STRIPPED_JAPANESE_MAPLE_POLE, "stripped_japanese_maple")
+    val STRIPPED_RAINBOW_EUCALYPTUS_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.STRIPPED_RAINBOW_EUCALYPTUS_POLE, "stripped_rainbow_eucalyptus")
+    val STRIPPED_SAKURA_POLE : BlockItem? = registerPoleItem(TerrestriaCompatBlocks.STRIPPED_SAKURA_POLE, "stripped_sakura")
 
     fun initItems() {
         if (Config.getBool("blocks.poles")) {
