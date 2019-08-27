367c3cb9f9a980b42a1092fe98b9b0e437ee95f3
RedstoneParadox <spinjitzu00@gmail.com>
2019-08-26 18:24:09 -0700

    Listener now checks if dynamite has been registered before injecting

--- src/main/java/redstoneparadox/nicetohave/misc/Listeners.kt
+++ src/main/java/redstoneparadox/nicetohave/misc/Listeners.kt
@@ -18,7 +18,7 @@ object Listeners {
 
     fun initListeners() {
         LootTableLoadingCallback.EVENT.register(LootTableLoadingCallback { resourceManager, manager, id, supplier, setter ->
-            if ("minecraft:chests/abandoned_mineshaft" == id.toString() && Config.getBool("items.dynamite")) {
+            if ("minecraft:chests/abandoned_mineshaft" == id.toString() && Items.DYNAMITE != null) {
                 val poolBuider = FabricLootPoolBuilder.builder()
                         .withFunction(SetCountLootFunction.builder(UniformLootTableRange.between(1.0f, 5.0f)))
                         .withEntry(ItemEntry.builder(Items.DYNAMITE).setWeight(8))
