fa048b7f8a9525e7793a36a33850698476811796
RedstoneParadox <spinjitzu00@gmail.com>
2019-10-03 15:57:40 -0700

    Added back the block render layer for chain link fences

--- src/main/java/redstoneparadox/nicetohave/client/NiceToHaveClient.kt
+++ src/main/java/redstoneparadox/nicetohave/client/NiceToHaveClient.kt
@@ -1,7 +1,9 @@
 package redstoneparadox.nicetohave.client
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.fabricmc.fabric.api.client.render.ColorProviderRegistry
+import net.minecraft.block.BlockRenderLayer
 import net.minecraft.client.color.block.BlockColorProvider
 import redstoneparadox.nicetohave.NiceToHave
 import redstoneparadox.nicetohave.block.Blocks
@@ -20,7 +22,9 @@ class NiceToHaveClient : ClientModInitializer {
         ColorProviderRegistry.BLOCK.register(BlockColorProvider { block, pos, world, layer ->
             val provider = ColorProviderRegistry.BLOCK.get(net.minecraft.block.Blocks.VINE)
             return@BlockColorProvider provider.getColor(block, pos, world, layer)
-            }, Blocks.TRIMMED_VINE)
+        }, Blocks.TRIMMED_VINE)
+
+        if (Blocks.CHAIN_LINK_FENCE != null) BlockRenderLayerMap.INSTANCE.putBlock(Blocks.CHAIN_LINK_FENCE, BlockRenderLayer.CUTOUT)
     }
 
 }
