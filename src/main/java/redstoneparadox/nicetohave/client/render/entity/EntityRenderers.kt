c93e5dfdffb7daa2a96f0a67b6a6ef87af0d9226
RedstoneParadox <spinjitzu00@gmail.com>
2019-12-11 19:24:34 -0800

    Dynamite entity type and renderer now respect the config

--- src/main/java/redstoneparadox/nicetohave/client/render/entity/EntityRenderers.kt
+++ src/main/java/redstoneparadox/nicetohave/client/render/entity/EntityRenderers.kt
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry
 import net.minecraft.client.render.entity.FlyingItemEntityRenderer
 import redstoneparadox.nicetohave.entity.EntityTypes
 import redstoneparadox.nicetohave.entity.ThrownDynamiteEntity
+import redstoneparadox.nicetohave.util.newconfig.Config
 
 /**
  * Created by RedstoneParadox on 5/24/2019.
@@ -12,7 +13,9 @@ object EntityRenderers {
 
 
     fun registerRenderers() {
-        val factory = EntityRendererRegistry.Factory { manager, context ->  FlyingItemEntityRenderer<ThrownDynamiteEntity>(manager, context.itemRenderer)}
-        EntityRendererRegistry.INSTANCE.register(EntityTypes.THROWN_DYNAMITE, factory)
+        if (Config.Items.dynamite) {
+            val factory = EntityRendererRegistry.Factory { manager, context ->  FlyingItemEntityRenderer<ThrownDynamiteEntity>(manager, context.itemRenderer)}
+            EntityRendererRegistry.INSTANCE.register(EntityTypes.THROWN_DYNAMITE, factory)
+        }
     }
 }
--- src/main/java/redstoneparadox/nicetohave/entity/EntityTypes.kt
+++ src/main/java/redstoneparadox/nicetohave/entity/EntityTypes.kt
@@ -5,6 +5,7 @@ import net.minecraft.entity.EntityCategory
 import net.minecraft.entity.EntityDimensions
 import net.minecraft.entity.EntityType
 import net.minecraft.util.registry.Registry
+import redstoneparadox.nicetohave.util.newconfig.Config
 
 /**
  * Created by RedstoneParadox on 5/23/2019.
@@ -14,7 +15,7 @@ object EntityTypes {
     val THROWN_DYNAMITE: EntityType<ThrownDynamiteEntity> = FabricEntityTypeBuilder.create<ThrownDynamiteEntity>(EntityCategory.MISC) { entityType, world ->  ThrownDynamiteEntity(entityType, world)}.size(EntityDimensions.fixed(0.25f, 0.25f)).trackable(4, 20).build()
 
     fun registerEntityTypes() {
-        Registry.register(Registry.ENTITY_TYPE, "nicetohave:thrown_dynamite", THROWN_DYNAMITE)
+        if (Config.Items.dynamite) Registry.register(Registry.ENTITY_TYPE, "nicetohave:thrown_dynamite", THROWN_DYNAMITE)
     }
 
 }
