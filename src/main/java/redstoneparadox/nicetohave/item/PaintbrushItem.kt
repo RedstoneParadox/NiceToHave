60cd26b20fd844dfbf3f012359f133061c084e11
RedstoneParadox <spinjitzu00@gmail.com>
2020-01-05 16:11:30 -0800

    Revamped paintbrush mechanics to consume dye as you use it

--- src/main/java/redstoneparadox/nicetohave/item/PaintbrushItem.kt
+++ src/main/java/redstoneparadox/nicetohave/item/PaintbrushItem.kt
@@ -1,26 +1,30 @@
 package redstoneparadox.nicetohave.item
 
+import net.minecraft.block.BlockState
 import net.minecraft.block.Blocks
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.DyeItem
 import net.minecraft.item.Item
 import net.minecraft.item.ItemUsageContext
-import net.minecraft.nbt.ByteTag
 import net.minecraft.tag.BlockTags
-import net.minecraft.tag.ItemTags
-import net.minecraft.tag.Tag
 import net.minecraft.util.ActionResult
 import net.minecraft.util.DyeColor
 import net.minecraft.util.Identifier
 import redstoneparadox.nicetohave.util.getBlock
-import java.lang.NullPointerException
 
 class PaintbrushItem(settings: Settings) : Item(settings) {
 
     init {
         addPropertyGetter(Identifier("color")) { stack, world, entity ->
-            val tag = stack.getOrCreateTag()["color"]
-            if (tag is ByteTag) {
-                val id = tag.int - 1
-                if (id >= 0) return@addPropertyGetter when (DyeColor.byId(id)) {
+            if (entity is PlayerEntity) {
+                val mainItem = entity.mainHandStack.item
+                val offhandItem = entity.offHandStack.item
+                val dye = when {
+                    mainItem is DyeItem -> mainItem
+                    offhandItem is DyeItem -> offhandItem
+                    else -> null
+                }
+                return@addPropertyGetter when (dye?.color) {
                     DyeColor.WHITE -> 0.01f
                     DyeColor.ORANGE -> 0.02f
                     DyeColor.MAGENTA -> 0.03f
@@ -37,7 +41,7 @@ class PaintbrushItem(settings: Settings) : Item(settings) {
                     DyeColor.GREEN -> 0.14f
                     DyeColor.RED -> 0.15f
                     DyeColor.BLACK -> 0.16f
-                    else -> 0.0f
+                    null -> 0.0f
                 }
             }
             return@addPropertyGetter 0.0f
@@ -47,38 +51,49 @@ class PaintbrushItem(settings: Settings) : Item(settings) {
     override fun useOnBlock(context: ItemUsageContext): ActionResult {
         val world = context.world
         val pos = context.blockPos
+        val player = context.player
         val block = world.getBlock(pos)
-        val tag = context.stack.getOrCreateTag()["color"]
 
-        if (tag is ByteTag && BlockTags.WOOL.contains(block)) {
-            val id = tag.int - 1
-            if (id >= 0) {
-                val newBlock = when (val color = DyeColor.byId(id)) {
-                    DyeColor.WHITE -> Blocks.WHITE_WOOL
-                    DyeColor.ORANGE -> Blocks.ORANGE_WOOL
-                    DyeColor.MAGENTA -> Blocks.MAGENTA_WOOL
-                    DyeColor.LIGHT_BLUE -> Blocks.LIGHT_BLUE_WOOL
-                    DyeColor.YELLOW -> Blocks.YELLOW_WOOL
-                    DyeColor.LIME -> Blocks.LIME_WOOL
-                    DyeColor.PINK -> Blocks.PINK_WOOL
-                    DyeColor.GRAY -> Blocks.GRAY_WOOL
-                    DyeColor.LIGHT_GRAY -> Blocks.LIGHT_GRAY_WOOL
-                    DyeColor.CYAN -> Blocks.CYAN_WOOL
-                    DyeColor.PURPLE -> Blocks.PURPLE_WOOL
-                    DyeColor.BLUE -> Blocks.BLUE_WOOL
-                    DyeColor.BROWN -> Blocks.BROWN_WOOL
-                    DyeColor.GREEN -> Blocks.GREEN_WOOL
-                    DyeColor.RED -> Blocks.RED_WOOL
-                    DyeColor.BLACK -> Blocks.BLACK_WOOL
-                    else -> block
+        if (player is PlayerEntity && BlockTags.WOOL.contains(block)) {
+            val mainItem = player.mainHandStack.item
+            val offhandItem = player.offHandStack.item
+            if (mainItem is DyeItem) {
+                val state = stateFromColor(mainItem.color)
+                if (state.block != block) {
+                    player.mainHandStack.decrement(1)
+                    world.setBlockState(pos, state)
+                    return ActionResult.SUCCESS
                 }
-                if (newBlock != block) {
-                    world.setBlockState(pos, newBlock.defaultState)
+            }
+            else if (offhandItem is DyeItem) {
+                val state = stateFromColor(offhandItem.color)
+                world.setBlockState(pos, state)
+                if (state.block != block) {
+                    player.offHandStack.decrement(1)
+                    world.setBlockState(pos, state)
                     return ActionResult.SUCCESS
                 }
             }
         }
-
         return ActionResult.FAIL
+    }
+
+    private fun stateFromColor(color: DyeColor): BlockState = when (color) {
+        DyeColor.WHITE -> Blocks.WHITE_WOOL.defaultState
+        DyeColor.ORANGE -> Blocks.ORANGE_WOOL.defaultState
+        DyeColor.MAGENTA -> Blocks.MAGENTA_WOOL.defaultState
+        DyeColor.LIGHT_BLUE -> Blocks.LIGHT_BLUE_WOOL.defaultState
+        DyeColor.YELLOW -> Blocks.YELLOW_WOOL.defaultState
+        DyeColor.LIME -> Blocks.LIME_WOOL.defaultState
+        DyeColor.PINK -> Blocks.PINK_WOOL.defaultState
+        DyeColor.GRAY -> Blocks.GRAY_WOOL.defaultState
+        DyeColor.LIGHT_GRAY -> Blocks.LIGHT_GRAY_WOOL.defaultState
+        DyeColor.CYAN -> Blocks.CYAN_WOOL.defaultState
+        DyeColor.PURPLE -> Blocks.PURPLE_WOOL.defaultState
+        DyeColor.BLUE -> Blocks.BLUE_WOOL.defaultState
+        DyeColor.BROWN -> Blocks.BROWN_WOOL.defaultState
+        DyeColor.GREEN -> Blocks.GREEN_WOOL.defaultState
+        DyeColor.RED -> Blocks.RED_WOOL.defaultState
+        DyeColor.BLACK -> Blocks.BLACK_WOOL.defaultState
     }
 }
