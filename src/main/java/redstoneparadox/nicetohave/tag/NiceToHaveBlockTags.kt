3665f2a792e5cc8ccb4c8cbbbcde548cb4151719
RedstoneParadox <spinjitzu00@gmail.com>
2020-01-07 12:00:24 -0800

    Added several more recipes to the paintbrush

--- src/main/java/redstoneparadox/nicetohave/tag/NiceToHaveBlockTags.kt
+++ src/main/java/redstoneparadox/nicetohave/tag/NiceToHaveBlockTags.kt
@@ -1,8 +1,14 @@
 package redstoneparadox.nicetohave.tag
 
 import net.fabricmc.fabric.api.tag.TagRegistry
+import net.minecraft.block.Block
+import net.minecraft.tag.Tag
 import net.minecraft.util.Identifier
 
 object NiceToHaveBlockTags {
-    val GLASS = TagRegistry.block(Identifier("nicetohave", "glass"))
+    val CONCRETE: Tag<Block> = TagRegistry.block(Identifier("nicetohave", "glass"))
+    val CONCRETE_POWDER: Tag<Block> = TagRegistry.block(Identifier("nicetohave", "glass"))
+    val GLASS: Tag<Block> = TagRegistry.block(Identifier("nicetohave", "glass"))
+    val GLASS_PANE: Tag<Block> = TagRegistry.block(Identifier("nicetohave", "glass"))
+    val TERRACOTTA: Tag<Block> = TagRegistry.block(Identifier("nicetohave", "glass"))
 }
