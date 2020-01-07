a9b5675af51ffce948b7d78cb880b022b5947f63
RedstoneParadox <spinjitzu00@gmail.com>
2020-01-07 11:11:30 -0800

    Cleanup

--- src/main/java/redstoneparadox/nicetohave/tag/NiceToHaveBlockTags.kt
+++ src/main/java/redstoneparadox/nicetohave/tag/NiceToHaveBlockTags.kt
@@ -1,8 +1,5 @@
 package redstoneparadox.nicetohave.tag
 
-import net.fabricmc.fabric.api.tag.FabricItemTags
-import net.fabricmc.fabric.api.tag.FabricTag
-import net.fabricmc.fabric.api.tag.FabricTagBuilder
 import net.fabricmc.fabric.api.tag.TagRegistry
 import net.minecraft.util.Identifier
 
