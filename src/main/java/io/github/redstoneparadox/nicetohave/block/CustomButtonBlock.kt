4d4b17f0afb97ff446b75ce54fc6e9c2a02f2bb3
RedstoneParadox <spinjitzu00@gmail.com>
2021-07-11 12:06:20 -0700

    Finally updated to 1.17

--- src/main/java/io/github/redstoneparadox/nicetohave/block/CustomButtonBlock.kt
+++ src/main/java/io/github/redstoneparadox/nicetohave/block/CustomButtonBlock.kt
@@ -6,7 +6,7 @@ import net.minecraft.block.Material
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
 
-class CustomButtonBlock(val ticks: Int): AbstractButtonBlock(false, FabricBlockSettings.of(Material.SUPPORTED).noCollision().strength(0.5f, 0.1f).breakByHand(true)) {
+class CustomButtonBlock(val ticks: Int): AbstractButtonBlock(false, FabricBlockSettings.of(Material.DECORATION).noCollision().strength(0.5f, 0.1f).breakByHand(true)) {
 
     override fun getClickSound(press: Boolean): SoundEvent {
         return if (press) SoundEvents.BLOCK_STONE_BUTTON_CLICK_ON else SoundEvents.BLOCK_STONE_BUTTON_CLICK_OFF
